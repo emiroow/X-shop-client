7a9b03b65d2dc3c4e79df25d3ff07006ead3f928
emiroow <amir_esmaeelzadeh@yahoo.com>
2025-04-18 13:39:52 +0330

    push product details

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,7 +1,11 @@
 import Breadcrumbs from "@/components/ui/Breadcrumbs";
+import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
+import { CiHeart } from "react-icons/ci";
+import { FaRegStar } from "react-icons/fa";
 import { FiMinus, FiPlus, FiRepeat } from "react-icons/fi";
 import { GoInfo } from "react-icons/go";
 import { HiOutlineTruck } from "react-icons/hi2";
+import { IoShareSocialOutline } from "react-icons/io5";
 import { LuBadgeCheck, LuCalendarRange } from "react-icons/lu";
 
 const Product = () => {
@@ -21,9 +25,7 @@ const Product = () => {
               <div className="flex items-center gap-x-3 mr-2">
                 <div className="tooltip">
                   <button className="rounded-full p-1.5 border-2 border-gray-200 dark:border-white/20">
-                    <svg className="w-5 h-5">
-                      <use href="#share"></use>
-                    </svg>
+                    <IoShareSocialOutline className="text-xl" />
                   </button>
                   <div className="tooltiptext duration-300 transition-all text-xs bg-zinc-700 dark:bg-zinc-500 text-white dark:to-zinc-700 after:border-t-zinc-700 after:dark:border-t-zinc-500">
                     اشتراک گذاری
@@ -31,15 +33,13 @@ const Product = () => {
                 </div>
                 <div className="tooltip ">
                   <button className="rounded-full p-1.5 border-2 border-gray-200 dark:border-white/20">
-                    <svg className="w-5 h-5">
-                      <use href="#heart"></use>
-                    </svg>
+                    <CiHeart className="text-xl" />
                   </button>
                   <div className="tooltiptext duration-300 transition-all text-xs bg-zinc-700 dark:bg-zinc-500 text-white dark:to-zinc-700 after:border-t-zinc-700 after:dark:border-t-zinc-500">
                     افزودن به علاقمندی
                   </div>
                 </div>
-                <div className="tooltip ">
+                {/* <div className="tooltip ">
                   <button className="rounded-full p-1.5 border-2 border-gray-200 dark:border-white/20">
                     <svg className="w-5 h-5">
                       <use href="#arrows-up-down"></use>
@@ -48,7 +48,7 @@ const Product = () => {
                   <div className="tooltiptext duration-300 transition-all text-xs bg-zinc-700 dark:bg-zinc-500 text-white dark:to-zinc-700 after:border-t-zinc-700 after:dark:border-t-zinc-500">
                     مقایسه
                   </div>
-                </div>
+                </div> */}
               </div>
               {/* <!-- SLIDER --> */}
               <div className="swiper mt-2 w-full porduct-details-slider rounded-lg">
@@ -83,9 +83,7 @@ const Product = () => {
                 </div>
                 <div className="flex gap-x-1">
                   <p className="font-DanaMedium mt-0.5">5.0</p>
-                  <svg className="w-5 h-5  text-yellow-400">
-                    <use href="#star"></use>
-                  </svg>
+                  <FaRegStar className="text-xl text-yellow-400" />
                 </div>
               </div>
               <div className="flex gap-x-1 text-gray-400 ">
@@ -190,6 +188,156 @@ const Product = () => {
             <GoInfo className="text-lg" />
             <p>ارسال رایگان برای خریدهای بالای 400 هزار تومان</p>
           </div>
+        </div>
+      </section>
+
+      {/* <!-- COMMENTS --> */}
+      <section className="w-full rounded-lg bg-white dark:bg-zinc-700 shadow p-5">
+        <div className="flex items-center gap-x-2 mb-6">
+          <h2 className="font-MorabbaMedium text-2xl ">دیدگاه ها</h2>
+          <p className="text-sm text-blue-500">(28 دیدگاه)</p>
+        </div>
+
+        <div className="flex flex-col lg:flex-row items-start gap-10">
+          {/* <!-- SUBMIT COMMENT --> */}
+          <div className="lg:w-1/4 flex flex-col w-full ">
+            <p className="font-DanaMedium text-lg mb-2">ثبت دیدگاه</p>
+            <input type="text" placeholder="عنوان" className="tailwind-input" />
+
+            <p className="text-gray-500 dark:text-white text-sm mb-4">
+              این محصول را به دیگران پیشنهاد :{" "}
+            </p>
+            <div
+              className="grid grid-cols-12 child:col-span-6 gap-4 child:w-full  child:flex child:items-center child:justify-center child:gap-x-2 child:rounded-lg child:shadow child:py-2 mb-5 child:font-DanaMedium child:duration-300 
+                        child:transition-all"
+            >
+              <button className="text-green-600 ring-transparent ring-1 focus:ring-green-600 dark:ring-white/20 dark:focus:ring-green-600">
+                <AiOutlineLike className="text-xl" />
+                میکنم
+              </button>
+              <button className="text-red-500 ring-transparent ring-1 focus:ring-[#EF4343] dark:ring-white/20 dark:focus:ring-[#EF4343]">
+                <AiOutlineDislike className="text-xl" />
+                نمیکنم
+              </button>
+            </div>
+            <textarea
+              className="h-24 tailwind-input"
+              placeholder="متن دیدگاه"
+            ></textarea>
+            <button className="rounded-lg p-2 bg-green-500 hover:bg-green-600 text-white transition-all">
+              ثبت
+            </button>
+          </div>
+
+          {/* <!-- ALL COMMENTS --> */}
+          <ul className="lg:w-3/4 flex flex-col gap-y-2 child:w-full  ">
+            {/* <!-- COMMENT ITEMS --> */}
+            <li className="child:flex py-4 border-b border-gray-200 child:border-white/20">
+              {/* <!-- TITLE --> */}
+              <div className="flex items-center gap-x-2">
+                <h2 className="font-DanaMedium text-lg mb-1">
+                  عطر و طعم قهوه عالی{" "}
+                </h2>
+                <span className="px-2 py-1 mb-2 rounded-lg bg-green-500 text-white text-xs">
+                  خریدار
+                </span>
+              </div>
+              {/* <!-- COOMENT TEXT --> */}
+              <div className="flex-col">
+                <h2 className="flex items-center gap-x-1 text-green-500 mb-4">
+                  <svg className="w-4 h-4">
+                    <use href="#hand-up"></use>
+                  </svg>
+                  پیشنهاد میشود
+                </h2>
+                <p className="text-gray-500 dark:text-gray-200 mb-2 line-clamp-2">
+                  عالی از هر لحاظ به شدت خریدش رو توصیه میکنم کیفیت محصول خوب
+                  بود بسته بندی عالی بود
+                </p>
+              </div>
+              {/* <!-- COMMENT FOOTER --> */}
+              <div className="mt-2 lg:mt-0 flex-col lg:flex-row gap-y-2 lg:items-center justify-between">
+                <div className="flex items-center gap-x-4 text-gray-400 text-sm">
+                  <p>11 بهمن 1402</p>
+                  <p>امیررضا کریمی</p>
+                </div>
+                <div className="flex items-center gap-x-2 flex-wrap mt-2">
+                  <p className="text-gray-400 text-sm">
+                    آیا این دیدگاه برایتان مفید بود؟
+                  </p>
+                  <div
+                    className="flex items-center gap-x-2 child:flex child:items-center child:gap-x-1 child:rounded-lg child:p-2 child:font-DanaMedium child:duration-300 
+                                child:transition-all child:text-sm"
+                  >
+                    <button className="text-green-600 ring-transparent ring-1 focus:ring-green-600  dark:focus:ring-green-600">
+                      78
+                      <AiOutlineLike className="text-lg" />
+                    </button>
+                    <button className="text-red-500 ring-transparent ring-1 focus:ring-[#EF4343]  dark:focus:ring-[#EF4343]">
+                      25
+                      <AiOutlineDislike className="text-lg" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+            {/* <!-- COMMENT ITEMS --> */}
+            <li className="child:flex py-4 border-b border-gray-200 child:border-white/20">
+              {/* <!-- TITLE --> */}
+              <div className="flex items-center gap-x-2">
+                <h2 className="font-DanaMedium text-lg mb-1">
+                  عطر و طعم قهوه عالی{" "}
+                </h2>
+                <span className="px-2 py-1 mb-2 rounded-lg bg-green-500 text-white text-xs">
+                  خریدار
+                </span>
+              </div>
+              {/* <!-- COOMENT TEXT --> */}
+              <div className="flex-col">
+                <h2 className="flex items-center gap-x-1 text-green-500 mb-4">
+                  <svg className="w-4 h-4">
+                    <use href="#hand-up"></use>
+                  </svg>
+                  پیشنهاد میشود
+                </h2>
+                <p className="text-gray-500 dark:text-gray-200 mb-2 line-clamp-2">
+                  عالی از هر لحاظ به شدت خریدش رو توصیه میکنم کیفیت محصول خوب
+                  بود بسته بندی عالی بود
+                </p>
+              </div>
+              {/* <!-- COMMENT FOOTER --> */}
+              <div className="mt-2 lg:mt-0 flex-col lg:flex-row gap-y-2 lg:items-center justify-between">
+                <div className="flex items-center gap-x-4 text-gray-400 text-sm">
+                  <p>11 بهمن 1402</p>
+                  <p>امیررضا کریمی</p>
+                </div>
+                <div className="flex items-center gap-x-2 flex-wrap mt-2">
+                  <p className="text-gray-400 text-sm">
+                    آیا این دیدگاه برایتان مفید بود؟
+                  </p>
+                  <div
+                    className="flex items-center gap-x-2 child:flex child:items-center child:gap-x-1 child:rounded-lg child:p-2 child:font-DanaMedium child:duration-300 
+                                child:transition-all child:text-sm"
+                  >
+                    <button className="text-green-600 ring-transparent ring-1 focus:ring-green-600  dark:focus:ring-green-600">
+                      78
+                      <AiOutlineLike className="text-lg" />
+                    </button>
+                    <button className="text-red-500 ring-transparent ring-1 focus:ring-[#EF4343]  dark:focus:ring-[#EF4343]">
+                      25
+                      <AiOutlineDislike className="text-lg" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <button className="more-comment-btn w-full flex items-center justify-center gap-x-1 my-4 text-green-600 font-DanaMedium">
+              <p className="more-comment-text">مشاهده بیشتر</p>
+              <svg className="w-4 h-4 more-comment-icon">
+                <use href="#chevron-down"></use>
+              </svg>
+            </button>
+          </ul>
         </div>
       </section>
     </div>
